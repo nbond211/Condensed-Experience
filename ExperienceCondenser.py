fd3d3ff9f4f9366f73502ec9b85ac1a0bc6bf2b7
nbond211 <nbond211@verizon.net>
2016-05-29 11:12:51 -0400

    method extraction

--- ExperienceCondenser.py
+++ ExperienceCondenser.py
@@ -1,34 +1,40 @@
 from random import randint
 import random
 from moviepy.editor import *
+import os
+import easygui
 
-video = VideoFileClip("test.mp4")
+def edit_video(videoFileLocation):
+    video = VideoFileClip(videoFileLocation)
 
-inputVideoDuration = int(video.duration)
-outputVideoDuration = 75
+    input_video_duration = int(video.duration)
+    output_video_duration = 75
 
-print inputVideoDuration
+    narrative_array = []
+    edit_times_array = []
 
-narrativeArray = []
-editTimesArray = []
+    for x in range(0, output_video_duration):
+        rand_time_in_sec = randint(0, input_video_duration - 1)
 
-for x in range (0, outputVideoDuration):
-    randTimeInSec = randint(0, inputVideoDuration - 1)
+        while rand_time_in_sec in edit_times_array:
+            rand_time_in_sec = randint(0, input_video_duration - 1)
 
-    while randTimeInSec in editTimesArray:
-        randTimeInSec = randint(0, inputVideoDuration - 1)
+        edit_times_array.append(rand_time_in_sec)
 
-    editTimesArray.append(randTimeInSec)
+    edit_times_array = sorted(edit_times_array)
 
-editTimesArray = sorted(editTimesArray)
+    for x in range(0, len(edit_times_array)):
+        rand_time_in_sec = edit_times_array[x]
+        rand_clip_duration = random.uniform(0.5, 1.0)
 
-for x in range (0, len(editTimesArray)):
-    randTimeInSec = editTimesArray[x]
-    randClipDuration = random.uniform(0.5, 1.0)
+        video_clip = video.subclip(rand_time_in_sec, rand_time_in_sec + rand_clip_duration)
+        narrative_array.append(video_clip)
 
-    videoClip = video.subclip(randTimeInSec, randTimeInSec + randClipDuration)
-    narrativeArray.append(videoClip)
+    final_video = concatenate_videoclips(narrative_array)
 
-finalVideo = concatenate_videoclips(narrativeArray)
+    newFileName = os.path.splitext(videoFileLocation)[0] + "_edited" + os.path.splitext(videoFileLocation)[1]
 
-finalVideo.write_videofile("test_edited.mp4",fps=25)
+    final_video.write_videofile(newFileName, fps=25)
+
+edit_video("test.mp4")
+
